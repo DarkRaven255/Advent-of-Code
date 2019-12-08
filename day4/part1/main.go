91cd37f15c9f9ea3a1e480a039dc5e4d251fd69c
Konrad Dulemba <konrad@dulemba.me>
2019-12-08 22:20:32 +0100

    Day 4: Secure Container [1/2]

---
+++ day4/part1/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.Open("file.txt")
+	if err != nil {
+		log.Println(err)
+	}
+	defer data.Close()
+
+	scanner := bufio.NewScanner(data)
+	scanner.Scan()
+
+	rangeStr := strings.Split(scanner.Text(), "-")
+
+	numberOfResults, err := findResults(rangeStr)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println("There are:", numberOfResults, "results")
+	}
+}
+
+func findResults(rangeStr []string) (int, error) {
+	start, err := strconv.Atoi(rangeStr[0])
+	if err != nil {
+		return -1, err
+	}
+	end, err := strconv.Atoi(rangeStr[1])
+	if err != nil {
+		return -1, err
+	}
+
+	var results int
+	for i := start; i < end; i++ {
+		adj := isAdj(i)
+		if adj {
+			if isInc(i) {
+				results++
+			}
+		}
+	}
+
+	return results, nil
+}
+
+func isAdj(num int) bool {
+	var curr int
+	curr = num % 10
+	num /= 10
+	for i := 0; ; i++ {
+		last := curr
+		curr = num % 10
+		num /= 10
+
+		if last == curr {
+			return true
+		}
+
+		if num == 0 {
+			break
+		}
+	}
+	return false
+}
+
+func isInc(num int) bool {
+	var curr int
+	curr = num % 10
+	num /= 10
+	for i := 0; ; i++ {
+		last := curr
+		curr = num % 10
+		num /= 10
+		if last < curr {
+			return false
+		}
+		if num == 0 {
+			break
+		}
+	}
+	return true
+}
